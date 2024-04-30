be43bd1526c6368b7c420106d0ac9232dd9f01be
PRASAD  BODKE <113661771+Prasad5102@users.noreply.github.com>
2024-04-30 13:46:05 +0530

    Update Non Repeating Character.java
    Given a string S consisting of lowercase Latin Letters. Return the first non-repeating character in S. If there is no non-repeating character, return '$'.
    
    Example 1:
    
    Input:
    S = hello
    Output: h
    Explanation: In the given string, the
    first character which is non-repeating
    is h, as it appears first and there is
    no other 'h' in the string.
    Example 2:
    
    Input:
    S = zxvczbtxyzvy
    Output: c
    Explanation: In the given string, 'c' is
    the character which is non-repeating. 
    Your Task:
    You only need to complete the function nonrepeatingCharacter() that takes string S as a parameter and returns the character. If there is no non-repeating character then return '$' .
    
    Expected Time Complexity: O(N).
    Expected Auxiliary Space: O(Number of distinct characters).
    Note: N = |S|
    
    Constraints:
    1 <= N <= 105

--- Non Repeating Character.java
+++ Non Repeating Character.java
@@ -1,1 +1,26 @@
+class Solution
+{
+    //Function to find the first non-repeating character in a string.
+    static char nonrepeatingCharacter(String S)
+    {
+        //Your code here
+       int[] number = new int[26];
+       char ch = ' ';
+       for (int i = 0; i < S.length(); i++) {
+             number[122 - S.charAt(i)]++;
+              }
+          for (int i = 0; i < S.length(); i++) {
+        if (number[122 - S.charAt(i)] == 1) {
+          ch = S.charAt(i);
+            break;
+       }
+          }
+        if (ch == ' ') {
+            int x=5;
+          return '$'; 
+           }
+           return ch; 
 
+    
+          }
+         }
